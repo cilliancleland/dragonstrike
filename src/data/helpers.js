a7ba3ee29c73c1f5a12789badaa900f5609f0724
cillian cleland <cilliancleland@gmail.com>
2023-12-27 20:00:42 +1100

    gh-pages

--- src/data/helpers.js
+++
@@ -1,6 +0,0 @@
-export const dragonCost = (dragon) => {
-  let cost = dragon.baseCost + dragon.advancedTraits.length + dragon.extraTraits.length;
-  let skillCost = dragon.advancedSkills.length + dragon.skills.length;
-  if (skillCost > 0) skillCost = skillCost -1;
-  return cost + skillCost;
-}
